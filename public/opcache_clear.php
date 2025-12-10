fb47fcfded897fbc25aaded23fc8cdf60f06702f
FabianHardy <fabian.hardy@trendyfoods.com>
2025-12-10 19:30:08 +0100

    Delete opcache_clear.php

--- public/opcache_clear.php
+++
@@ -1,8 +0,0 @@
-<?php
-// opcache_clear.php - SUPPRIMER APRÈS USAGE
-if (function_exists('opcache_reset')) {
-    opcache_reset();
-    echo "OPcache vidé !";
-} else {
-    echo "OPcache non disponible";
-}
